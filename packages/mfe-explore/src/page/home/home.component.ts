8ad729b6662c234c0da8f7502b1a50839153769f
JesusJs <jdchavezzss@gmail.com>
2026-05-23 09:49:22 -0400

    Se implemento la logica necesaria para el funcionamiento del mfe en html, se agregaron metods para el agregado del cart, se agregaron metodos para cargar inventarios, se agregaron metodos para cargar recomendaciones

--- packages/mfe-explore/src/page/home/home.component.ts
+++ packages/mfe-explore/src/page/home/home.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { CatalogService, CategoryTeaser } from '@the-tractor-store/shared-catalog';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,18 @@ import { RouterLink } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  // Imágenes para los productos
-    classicTractorImage = "https://images.unsplash.com/photo-1564121319696-13b543ef4df4?w=600&auto=format&fit=crop";
-    autonomousTractorImage = "https://images.unsplash.com/photo-1581094240067-3bb9b63d78a3?w=600&auto=format&fit=crop";
+  private catalogService = inject(CatalogService);
+
+  public teasers = signal<CategoryTeaser[]>([]);
+  public isLoading = signal(true);
+
+  constructor() {
+    this.catalogService.getHome().subscribe({
+      next: (data) => {
+        this.teasers.set(data.teasers);
+        this.isLoading.set(false);
+      },
+      error: () => this.isLoading.set(false),
+    });
+  }
 }
