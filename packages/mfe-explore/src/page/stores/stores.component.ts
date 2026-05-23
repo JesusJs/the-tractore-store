0ef13a60fc2ff3714f322100a60729e52bc35697
JesusJs <jdchavezzss@gmail.com>
2026-05-23 09:55:17 -0400

    Se implemento logica para el funcionamiento de los stores

--- packages/mfe-explore/src/page/stores/stores.component.ts
+++ packages/mfe-explore/src/page/stores/stores.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CatalogService, StoreLocation } from '@the-tractor-store/shared-catalog';
 
 @Component({
   selector: 'app-stores',
@@ -8,4 +9,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './stores.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StoresComponent {}
+export class StoresComponent {
+  private catalogService = inject(CatalogService);
+
+  public stores = signal<StoreLocation[]>([]);
+  public isLoading = signal(true);
+
+  constructor() {
+    this.catalogService.getStores().subscribe({
+      next: (data) => {
+        this.stores.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => this.isLoading.set(false),
+    });
+  }
+}
