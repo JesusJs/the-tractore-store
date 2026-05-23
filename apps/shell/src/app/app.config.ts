3aec8213117616d32a532b3c2ab132c6df68ac1a
JesusJs <jdchavezzss@gmail.com>
2026-05-22 21:40:07 -0400

    implementando los provideHttpClient y el withInterceptors de angular common http

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './app.routes';
+import { mockBackendInterceptor, httpErrorInterceptor } from '@the-tractor-store/shared-catalog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
+    provideHttpClient(withInterceptors([mockBackendInterceptor, httpErrorInterceptor])),
   ],
 };
