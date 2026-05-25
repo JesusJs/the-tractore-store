d7166c5bbc36db47cd4031f92af1ad81195a9b53
JesusJs <jdchavezzss@gmail.com>
2026-05-24 21:16:48 -0500

    mejora en configuracion del app.config.ts para el providedHttpClient y el withInterceptors

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './app.routes';
-import { mockBackendInterceptor, httpErrorInterceptor } from '@the-tractor-store/shared-catalog';
+import { httpErrorInterceptor } from '@the-tractor-store/shared-catalog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([mockBackendInterceptor, httpErrorInterceptor])),
+    provideHttpClient(withInterceptors([httpErrorInterceptor])),
   ],
 };
